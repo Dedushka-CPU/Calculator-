b112924b60aa033fe5e96ab059e4de247e6c5661
_Dedushka_ <zomb689@gmail.com>
2025-02-27 20:29:15 +0400

    Delete calc.h

--- calc.h
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-#include <string>
-#include <vector>
-#include <cctype>
-#include <stack>
-
-bool isExpressionValid(const std::string& str);
-double Calculate(const std::string& str);
-double CalculateBrackets(std::string str);
